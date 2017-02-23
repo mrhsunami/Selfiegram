d444109018826717899ae69a2d0fd6c04dde5960
Nathan Hsu <nathan.hsu@icloud.com>
2017-02-22 19:07:41 -0800

    Initial Commit

---
+++ Selfiegram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Selfiegram
+//
+//  Created by Nathan Hsu on 2017-02-22.
+//  Copyright © 2017 Nathan Hsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
